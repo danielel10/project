09f4db76a7d8b55b0ef4fd05f8fc6ef5cd866429
daniel-ubuntu <elhadan@post.bgu.ac.il>
2023-02-04 20:57:28 +0200

    BE - added support to functions

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-
+from flask import Flask, jsonify, request
 
 # configuration
 DEBUG = True
@@ -17,6 +17,41 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
+
+
+molecules = [
+    {
+        'fName': 'On the Road',
+        'Mass': 'Jack Kerouac',
+        'Plot': True
+    },
+    {
+        'fName': 'Harry Potter and the Philosopher\'s Stone',
+        'Mass': 'J. K. Rowling',
+        'Plot': False
+    },
+    {
+        'fName': 'Green Eggs and Ham',
+        'Mass': 'Dr. Seuss',
+        'Plot': True
+    }
+]
+
+
+@app.route('/Molecules', methods=['GET'])
+def all_molecules():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        molecules.append({
+            'fName': post_data.get('fName'),
+            'Mass': post_data.get('Mass'),
+            'Plot': post_data.get('Plot')
+        })
+        response_object['message'] = 'molecule added!'
+    else:
+        response_object['molecules'] = molecules
+    return jsonify(response_object)
 
 
 if __name__ == '__main__':
